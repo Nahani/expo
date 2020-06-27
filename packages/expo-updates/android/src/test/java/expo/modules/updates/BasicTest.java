a96c20966b4a674e60b10412f08fe5d2761fc98a
Eric Samelson <eric@expo.io>
2020-06-26 17:38:40 -0700

    [expo-updates][android] add a few basic unit tests

---
+++ packages/expo-updates/android/src/test/java/expo/modules/updates/BasicTest.java
@@ -0,0 +1,11 @@
+package expo.modules.updates;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BasicTest {
+  @Test
+  public void basicTestAddition() {
+    Assert.assertTrue(2+2 == 4);
+  }
+}
